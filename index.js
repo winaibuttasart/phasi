d9754789f54cab3382feeeeef3fc1a1e7d26ea19
winaibuttasart <winai.bu.58@ubu.ac.th>
2017-03-25 23:54:09 +0700

    update

--- index.js
+++ index.js
@@ -29,18 +29,35 @@ app.listen(app.get('port'), function() {
 });
 
 
+// var salary = req.query.salary; // $_GET["salary"]
+// salary *= 12;                  //test use data
+// var gnder = req.query.gender;  //test get data
+
 app.get('/bot', function(req, res) {
+    var jsonResponse = [];
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query;
-    // var salary = req.query.salary; // $_GET["salary"]
-    // salary *= 12;
-    // var gnder = req.query.gender;
-    var no = req.query.no1;
+    var no_1 = req.query.no1;
+    var no_2 = req.query.no2;
+    var no_3 = req.query.no3;
+    var no_4 = req.query.no4;
+    var no_5 = req.query.no5;
+    var no_6 = req.query.no6;
+    var no_7 = req.query.no7;
+    var no_8 = req.query.no8;
 
-    console.log(no);
+    console.log(no_1);
 
-    var jsonResponse = [];
-    jsonResponse.push({"text" : "No1 : "+no});
+
+    jsonResponse.push({"text" : "No1 : "+no_1});
+    jsonResponse.push({"text" : "No2 : "+no_2});
+    jsonResponse.push({"text" : "No3 : "+no_3});
+    jsonResponse.push({"text" : "No4 : "+no_4});
+    jsonResponse.push({"text" : "No5 : "+no_5});
+    jsonResponse.push({"text" : "No6 : "+no_6});
+    jsonResponse.push({"text" : "No7 : "+no_7});
+    jsonResponse.push({"text" : "No8 : "+no_8});
+    jsonResponse.push({"text" : "กรอกข้อมูลลดหย่อนภาษี"});
   //  jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..."+gnder+"  salary  = "+salary});
 
     res.send(jsonResponse);
