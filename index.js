c53d398fd9c86af23297489b01dd86c89fdf5358
winaibuttasart <winai.bu.58@ubu.ac.th>
2017-03-25 15:38:00 +0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var cool =require('cool-ascii-faces');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+
+app.use(bodyParser.json());
+app.set('port', (process.env.PORT || 4000));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/public'));
 
