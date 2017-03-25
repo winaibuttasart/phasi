b79361fc2ad1216501dae8464e031e27ad2441a1
winaibuttasart <winai.bu.58@ubu.ac.th>
2017-03-25 13:55:48 +0700

    update

--- index.js
+++ index.js
@@ -30,5 +30,6 @@ app.get('/bot', function(req, res) {
   //  var jsonResponse = [];
   //  jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
     //res.send(jsonResponse);
+    console.logs(res);
     res.send(res);
 });
